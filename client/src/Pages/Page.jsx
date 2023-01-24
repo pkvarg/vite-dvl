4a0d519a9809bf4bff93ec078d8618adca233da9
Peter Varga <pkvarg@yahoo.se>
2023-01-24 16:21:13 +0100

    Cookies 01

--- client/src/Pages/Page.jsx
+++ client/src/Pages/Page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SixSections, ContactForm, Footer, ScrollToTop } from '../components'
+import CookieConsent from 'react-cookie-consent'
 
 const Page = () => {
   return (
@@ -7,6 +8,36 @@ const Page = () => {
       <SixSections />
       <ContactForm />
       <ScrollToTop />
+      <CookieConsent
+        location='bottom'
+        style={{
+          background: '#adbbcb',
+          color: '#000',
+          textAlign: 'left',
+          fontSize: '22.5px',
+        }}
+        buttonStyle={{
+          background: '#1d9f2f',
+          color: '#fff',
+          fontSize: '17.5px',
+        }}
+        buttonText='Súhlasím'
+        expires={365}
+        enableDeclineButton
+        onDecline={() => {
+          alert('nay!')
+        }}
+        declineButtonStyle={{
+          background: 'red',
+          color: '#fff',
+          fontSize: '17.5px',
+        }}
+        declineButtonText='Nesúhlasím'
+      >
+        Táto stránka používa len analytické a pre fungovanie webu nevyhnutné
+        cookies. Nepoužívame funkčné ani marketingové cookies.{' '}
+        <a href='/gdpr'> GDPR</a>
+      </CookieConsent>
       <Footer />
     </>
   )
