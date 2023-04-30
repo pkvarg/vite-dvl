01d287442022b21f978a2d5f77b0ab2859a47d60
peter varga <pkvarg@yahoo.se>
2023-04-30 11:41:19 +0200

    analytics corr

--- client/src/Pages/Page.jsx
+++ client/src/Pages/Page.jsx
@@ -1,19 +1,9 @@
 import React, { useState } from 'react'
 import { SixSections, ContactForm, ScrollToTop, Footer } from '../components'
 import CookieConsent from 'react-cookie-consent'
-import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 import axios from 'axios'
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-}
+import { app } from './../App'
 
 const Page = () => {
   const [cookieAccept, setCookieAccept] = useState(false)
@@ -38,12 +28,10 @@ const Page = () => {
       // `http://localhost:2000/api/visitors/dvl/agree/increase`,
       config
     )
-    console.log('vstrsAgr:', data.visitorsAgreedDvl)
   }
 
   // Initialize Firebase
   if (cookieAccept) {
-    const app = initializeApp(firebaseConfig)
     const analytics = getAnalytics(app)
   }
 
